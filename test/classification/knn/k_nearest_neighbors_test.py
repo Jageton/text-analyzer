d29b855158bc99f782ec259022eee99e1ec83721
antonRyzhkov <ryzhkovant@gmail.com>
2021-05-25 22:26:13 +0300

    рефакторинг KNN

--- test/classification/knn/k_nearest_neighbors_test.py
+++ test/classification/knn/k_nearest_neighbors_test.py
@@ -1,10 +1,28 @@
+from classification.knn.k_nearest_neighbors import KNN
 import unittest
-
-
+from pandas.core.frame import DataFrame
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
 class KNearestNeighborsTest(unittest.TestCase):
 
     def test_alg(self):
-        self.assertEqual(1, 1)
+
+        iris = datasets.load_iris()
+        iris_frame = DataFrame(iris.data)
+        iris_frame.columns = iris.feature_names
+        iris_frame['target'] = iris.target
+        iris_frame['name'] = iris_frame.target.apply(
+            lambda x: iris.target_names[x])
+        iris_frame = iris_frame.drop(columns=["name"])
+
+        X = iris_frame.drop(columns=['target'])
+        Y = iris_frame.values
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.25, random_state=1)
+        classifier=KNN(n_neighbors=10)
+        predict=classifier.run(X_train,Y_train,X_test)
+        self.assertEqual(np.array_equal(Y_test,predict),True)
 
 
 if __name__ == '__main__':
